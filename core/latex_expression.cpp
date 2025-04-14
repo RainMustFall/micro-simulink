01b34786edac40ba1c38de8e06a12447a045da7e
Dzmitry Karshakevich <dim.korsh2011@yandex.ru>
2025-04-14 22:24:32 +0200

    Support different priorities of operations when generating Latex

--- core/latex_expression.cpp
+++ core/latex_expression.cpp
@@ -2,22 +2,35 @@
 
 #include <sstream>
 
-LatexExpression::LatexExpression(std::string expression)
-    : expression_(std::move(expression)) {}
+LatexExpression::LatexExpression(std::string expression,
+                                 Priority last_operation_priority)
+    : expression_(std::move(expression)),
+      last_operation_priority_(last_operation_priority) {}
 
 std::unique_ptr<LatexExpression> LatexExpression::operator+(
     const LatexExpression& rhs) {
-  return std::make_unique<LatexExpression>(expression_ + '+' + rhs.expression_);
+  return std::make_unique<LatexExpression>(expression_ + '+' + rhs.expression_,
+                                           Priority::Additive);
 }
 
 std::unique_ptr<LatexExpression> LatexExpression::Integrate(
     double lower_limit, double upper_limit) const {
   std::ostringstream ss;
-  ss << "\\int_{" << lower_limit << "}^{" << upper_limit << "}\\left("
-     << expression_ << "\\right)dx";
-  return std::make_unique<LatexExpression>(ss.str());
+  ss << "\\int_{" << lower_limit << "}^{" << upper_limit << "}"
+     << MaybePutParentheses(expression_, Priority::Multiplicative) << "dx";
+  return std::make_unique<LatexExpression>(ss.str(), Priority::Multiplicative);
 }
 
 const std::string& LatexExpression::GetExpression() const {
   return expression_;
+}
+
+std::string LatexExpression::MaybePutParentheses(std::string expression,
+                                                 Priority new_priority) const {
+  if (static_cast<int>(new_priority) >
+      static_cast<int>(last_operation_priority_)) {
+    return "\\left(" + expression + "\\right)";
+  }
+
+  return std::move(expression);
 }
--- core/latex_expression.h
+++ core/latex_expression.h
@@ -4,9 +4,12 @@
 #include <memory>
 #include <string>
 
+enum class Priority { Additive = 0, Multiplicative = 1, Expression = 2 };
+
 class LatexExpression {
  public:
-  LatexExpression(std::string expression);
+  LatexExpression(std::string expression,
+                  Priority last_operation_priority = Priority::Expression);
   std::unique_ptr<LatexExpression> operator+(const LatexExpression& rhs);
 
   virtual std::unique_ptr<LatexExpression> Integrate(double lower_limit,
@@ -15,7 +18,10 @@ class LatexExpression {
   const std::string& GetExpression() const;
 
  private:
+  std::string MaybePutParentheses(std::string expression,
+                                  Priority new_priority) const;
   std::string expression_;
+  Priority last_operation_priority_;
 };
 
 #endif  // LATEX_EXPRESSION_H
